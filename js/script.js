a4cc948a9ae2fd43b362c5097d53b7d25f9ea5bd
rahman-anika <anikarahman37@gmail.com>
2021-09-02 00:07:30 +0600

    link modified

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ const searchBook = () => {
 const loadBooks = (searchText) => {
 
 
-    const url = ` http://openlibrary.org/search.json?q=${searchText}`;
+    const url = ` https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayBooks(data.docs));
